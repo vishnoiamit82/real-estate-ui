157d4d4d88c59419fb1150ef042a840fd7fbb72c
Amit Vishnoi <amitvishnoi@Amits-MacBook-Pro.local>
2025-03-06 20:12:48 +1100

    front end updates

--- src/components/PropertyForm.js
+++ src/components/PropertyForm.js
@@ -263,7 +263,7 @@ const PropertyForm = () => {
 
         <div className="container mx-auto p-4 max-w-3xl sm:max-w-6xl">
 
-            {/* Create Property Header Section */}
+            {/* reate Property Header Section */}
             <div className="container mx-auto p-4 max-w-3xl sm:max-w-6xl">
         <h2 className="text-xl sm:text-2xl font-semibold bg-green-500 text-white px-4 py-2 rounded-lg inline-flex items-center">
             <PlusCircle className="w-5 h-5 mr-2 text-white" />
@@ -523,7 +523,7 @@ const PropertyForm = () => {
                             type="submit"
                             className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
                         >
-                            Create Property
+                            Save Property
                         </button>
                         {/* Success Message (Now Below Button) */}
                         {/* Success & Error Messages */}
